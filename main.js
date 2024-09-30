9025bb94caa48f0735a25b630d6f1157719950ff
gerardmoreno123 <gerardmoreno@iesebre.com>
2024-09-30 19:34:52 +0200

    Deleted paralx, created main.js

--- main.js
+++ main.js
@@ -1,24 +1,51 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+// main.js
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const scrollToTopButton = document.getElementById('scrollToTop');
 
-setupCounter(document.querySelector('#counter'))
+// Mostrar/ocultar el botón al hacer scroll
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) { // Muestra el botón después de 300px de desplazamiento
+    scrollToTopButton.classList.remove('hidden');
+  } else {
+    scrollToTopButton.classList.add('hidden');
+  }
+});
+
+// Función para desplazarse al tope de la página con un efecto suave
+scrollToTopButton.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
+
+function handleClick() {
+  alert('¡Botón presionado!');
+}
+
+const words = ['Full Stack Dev.', 'Software Dev.', 'Web Designer.', 'Programmer.'];
+let index = 0;
+const dynamicText = document.getElementById('dynamic-text');
+
+function typeWriter() {
+  if (index < words.length) {
+    dynamicText.textContent = words[index];
+    index++;
+  } else {
+    index = 0;
+  }
+  setTimeout(typeWriter, 2000);
+}
+
+typeWriter();
+
+window.addEventListener('scroll', function() {
+  const scrollY = window.scrollY;
+
+  document.querySelectorAll('.parallax-cube').forEach((cube, index) => {
+    const speed = 0.5 + index * 0.15; // Ajusta la velocidad de cada cubo
+    const yPosition = scrollY * speed;
+
+    cube.style.transform = `translateY(-${yPosition}px)`; // Mueve el cubo hacia arriba
+  });
+});
